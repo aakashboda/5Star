904481f98008582d7e2df7de861cc11771ae5097
Aakash <bodaakash@gmail.com>
2024-07-25 11:48:39 +0530

    Company Module Added

---
+++ 5Star.Server/Controllers/ComanyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _5Star.Server.Controllers
+{
+    public class ComanyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
